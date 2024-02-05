5ebfea8f16a6a92978ace00f916f9fe861092193
Joshua <bigginsjoshua@gmail.com>
2024-02-05 13:50:19 +1100

    Completed clk_counter project.

--- apio/icestick/clk_counter/clk_counter.v
+++ apio/icestick/clk_counter/clk_counter.v
@@ -2,7 +2,7 @@
 module button_counter (
 
     // Oscillator
-    input               clk,
+    input               clk,    // 12MHz oscillator
 
     // Inputs
     input               rst_btn,
@@ -11,13 +11,30 @@ module button_counter (
     output  reg [3:0]   led
 );
 
-    wire rst;
+    wire            rst;
+    reg      [23:0] osc_counter;   // Counts oscillator
+    reg      [0:0] slow_clk;      // 1Hz clock
 
     // Reset is the inverse of the reset button
     assign rst = ~rst_btn;
 
-    // Count up on clock rising edge or reset on button push
+    // Divide oscillator signal to get 1Hz clock
     always @ (posedge clk or posedge rst) begin
+        if (rst == 1'b1) begin
+            slow_clk <= 1'b0;
+            osc_counter <= 24'b0;
+        end else begin
+            if (osc_counter >= 12000000) begin
+                slow_clk <= slow_clk + 1'b1;
+                osc_counter <= 24'b0;
+            end else begin
+                osc_counter <= osc_counter + 1'b1;
+            end
+        end
+    end
+
+    // Count up on clock rising edge or reset on button push
+    always @ (posedge slow_clk or posedge rst) begin
         if (rst == 1'b1) begin
             led <= 4'b0;
         end else begin
