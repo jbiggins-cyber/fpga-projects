6cc28b5f019450804a5544b529f5fed47f62776a
Joshua <bigginsjoshua@gmail.com>
2024-02-05 13:12:01 +1100

    Started clock counter projects.

--- apio/icestick/clk_counter/clk_counter.v
+++ apio/icestick/clk_counter/clk_counter.v
@@ -1,21 +1,20 @@
 // Module: Count up on each button press and display on LEDs
 module button_counter (
 
+    // Oscillator
+    input               clk,
+
     // Inputs
-    input       [1:0]   pmod,
+    input               rst_btn,
 
     // Outputs
     output  reg [3:0]   led
 );
 
     wire rst;
-    wire clk;
 
-    // Reset is the inverse of the first button
-    assign rst = ~pmod[0];
-
-    // Clock signal is the inverse of the second button
-    assign clk = ~pmod[1];
+    // Reset is the inverse of the reset button
+    assign rst = ~rst_btn;
 
     // Count up on clock rising edge or reset on button push
     always @ (posedge clk or posedge rst) begin
